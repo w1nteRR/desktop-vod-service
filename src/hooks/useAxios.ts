c1df5e2b0586992c081d745c7dce76b91afb1fe6
Vatamanyuk Mykola <mykola796@gmail.com>
2020-07-22 19:35:24 +0300

    axios hook added

---
+++ src/hooks/useAxios.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react'
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
+
+export const useAxios = (url: string, options: AxiosRequestConfig) => {
+
+    const [res, setRes] = useState<AxiosResponse | null>(null)
+    const [loading, setLoading] = useState(true)
+    const [refresh, setRefresh] = useState(false)
+
+    useEffect(() => {
+        let cleanUp = false
+        
+        const fetch = async () =>{
+            try {
+
+                const res = await axios(`${url}`, options)
+
+                if(!cleanUp) {
+                    setRes(res)
+                    setLoading(false)
+                }
+
+            } catch (err) {
+                console.log(err)
+            }
+        }
+
+        fetch()
+        setLoading(true)
+
+        return () => {
+            cleanUp = true
+        }
+
+    }, [url, refresh])
+
+
+    const refreshFetch = () => setTimeout(() => setRefresh(prevState => !prevState), 1000)
+     
+    return {
+        res, loading, refreshFetch
+    }
+}
