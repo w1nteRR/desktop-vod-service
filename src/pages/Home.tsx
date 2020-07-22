4ffbb0038d5aecc9cab7a14512f6684a66231281
Vatamanyuk Mykola <mykola796@gmail.com>
2020-07-22 19:37:22 +0300

    home page added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,37 @@
 import React, { FC } from 'react'
 
-import { PageLayout } from '../components/shared/utils/layout'
+import { PageLayout, Container } from '../components/shared/utils/layout'
+import { DualRing } from '../components/shared/styled/loaders/DualRing'
+
 import { FilmCarousel } from '../components/Carousels/Films.carousel'
 
+import { useAxios } from '../hooks/useAxios'
+
+import { IPlaylist } from '../interfaces/Playlist/IPlaylist'
+
 export const Home: FC = () => {
+
+    const { res, loading } = useAxios(`/api/video/playlists/${0}`, {
+        method: 'GET'
+    })
+
+    if(loading) {
+        return (
+            <Container h='90vh'>
+                <DualRing />
+            </Container>
+        )
+    }
+        
+    const playlists: Array<IPlaylist> = res?.data
+
     return (
         <PageLayout>
-            <FilmCarousel />
-            <FilmCarousel />
-            <FilmCarousel />
-            <FilmCarousel />
-            <FilmCarousel />
-            <FilmCarousel />
-            <FilmCarousel />
+            {
+                playlists.map(playlist => 
+                    <FilmCarousel key={playlist._id} name={playlist.name} films={playlist.films} />
+                )
+            }
         </PageLayout>
     )
 }
