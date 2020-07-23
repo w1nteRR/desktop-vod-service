2958becce8d0528384ccb72e4535ac18a65c3c0e
Vatamanyuk Mykola <mykola796@gmail.com>
2020-07-23 20:55:53 +0300

    buttons reworked

---
+++ src/components/shared/styled/buttons/Button.styled.ts
@@ -0,0 +1,73 @@
+import styled from 'styled-components'
+
+import { text } from '../../utils/colors'
+
+interface ButtonStyled {
+    w?: string,
+    h?: string,
+    bgColor?:string
+}
+
+
+export const Button = styled.button<ButtonStyled>`
+    width: ${props => props.w || 'auto'};
+    height: ${props => props.h || '35px'};
+    
+    background-color: ${props => props.bgColor || 'transparent'};
+
+    outline: none;
+    border: none;
+
+    cursor: pointer;
+`
+
+export const WatchS = styled(Button)`
+    width: 200px;
+    height: 45px;
+
+    margin: 10px;
+    
+    box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.25);
+
+    &:hover {
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
+        transition: box-shadow 1s ease;
+    }
+`
+
+export const BackS = styled(Button)`
+    width: 30px;
+    height: 30px;
+
+    border-radius: 50%;
+    
+    margin: 10px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &:hover {
+        background: ${text.silver};
+        transition: .5s background-color ease;
+    }
+`
+
+export const WatchLaterS = styled(Button)`
+    width: 45px;
+    height: 45px;
+
+    margin: 10px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+    background-color: ${text.dark};
+`
+
+
+
+
+
+
---
+++ src/components/shared/styled/buttons/Buttons.shared.tsx
@@ -0,0 +1,38 @@
+import React, { FC } from 'react'
+import Icon from '@mdi/react'
+import { mdiHistory, mdiChevronLeft } from '@mdi/js'
+
+import { text } from '../../utils/colors'
+import { Title } from '../../utils/typography'
+
+import { BackS, WatchS, WatchLaterS} from '../buttons/Button.styled'
+
+
+interface ButtonProps {
+    onClick(): void
+}
+
+interface ButtonWl extends ButtonProps {
+    isWatchLater: boolean
+}
+
+
+export const ButtonWl: FC<ButtonWl> = ({ onClick, isWatchLater }) => 
+    <WatchLaterS onClick={onClick}>
+        <Icon path={mdiHistory} size={.7} color={isWatchLater ? text.red : '#fff'} />
+    </WatchLaterS> 
+
+export const ButtonWatch: FC<ButtonProps> = ({ onClick }) =>
+    <WatchS onClick={onClick}>
+        <Title>Watch now</Title>
+    </WatchS>
+
+export const ButtonBack: FC<ButtonProps> = ({ onClick }) =>
+    <BackS onClick={onClick}>
+        <Icon path={mdiChevronLeft} size={1} color='#fff' />
+    </BackS>
+    
+    
+
+
+
