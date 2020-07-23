98409c267153ca3c9e69812db45ea889f935f97e
Vatamanyuk Mykola <mykola796@gmail.com>
2020-07-23 22:14:13 +0300

    film page update

--- src/pages/Film.tsx
+++ src/pages/Film.tsx
@@ -7,10 +7,16 @@ import { DualRing } from '../components/shared/styled/loaders/DualRing'
 import { Header } from '../components/Film/Header'
 import { Wallpaper } from '../components/Film/Wallpaper/Wallpaper'
 import { Cast } from '../components/Film/Cast/Cast'
+import { Control } from '../components/Film/Control/Control'
+import { Info } from '../components/Film/Info/Info'
+import { Simialr } from '../components/Film/Similar/Similar'
+import { Series } from '../components/Film/Series/Series'
 
 import { useAxios } from '../hooks/useAxios'
 
 import { IFilm } from '../interfaces/film/IFilm'
+import { IFilmCard } from '../interfaces/film/IFilm.card'
+
 
 interface MatchParams {
     id: string
@@ -33,12 +39,28 @@ export const Film: FC<FilmProps> = ({ match }) => {
     }
 
     const film: IFilm = res?.data.film
+    const similar: Array<IFilmCard> = res?.data.similar
 
     return (
         <PageLayout>
             <Header name={film.name} />
             <Wallpaper wallpaper={film.wallpaper} />
+            <Control />
             <Cast cast={film.cast} />
+            {
+                film.type === 'serial' && <Series series={film.series} />
+            }
+            <Info
+                audio={film.audio}
+                company={film.company}
+                country={film.country}
+                director={film.director}
+                genr={film.genr}
+                release={film.release}
+                subtitles={film.subtitles}
+                year={film.year.toString()}
+            />
+            <Simialr similar={similar} />
         </PageLayout>
     )
 }
