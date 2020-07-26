c1326e087019bb62603ad50bac0f5a8230fd7422
Vatamanyuk Mykola <mykola796@gmail.com>
2020-07-26 21:37:20 +0300

    search reducer update

--- src/redux/search/types.ts
+++ src/redux/search/types.ts
@@ -1,11 +1,22 @@
 import { ThunkAction } from 'redux-thunk'
 
 import { IFilmCard } from '../../interfaces/film/IFilm.card'
+import { IFilter, ISearchData } from '../../interfaces/filter/IFilter'
+import { RootState } from '../rootReducer'
 
 export const GET_FILMS_BY_NAME = 'GET_FILMS_BY_NAME'
+export const INIT_TAGS = 'INIT_TAGS'
+export const SET_ACTIVE_TAG = 'SET_ACTIVE_TAG'
+export const SET_SEARCH_DATA = 'SET_SEARCH_DATA'
 
 export interface SearchState {
-    result: Array<IFilmCard>
+    result: Array<IFilmCard>,
+    tags: {
+        genr: Array<IFilter>
+        company: Array<IFilter>
+        year: Array<IFilter>
+    }
+    searchData: ISearchData
 }
 
 interface GetFilmsByNameAction {
@@ -13,6 +24,32 @@ interface GetFilmsByNameAction {
     payload: Array<IFilmCard>
 }
 
-export type SearchActionTypes = GetFilmsByNameAction
+interface InitTagsAction {
+    type: typeof INIT_TAGS,
+    payload: {
+        genr: Array<IFilter>,
+        company: Array<IFilter>
+        year: Array<IFilter>
+    }
+}
 
-export type SearchThunkType = ThunkAction<Promise<void>, SearchState, unknown, SearchActionTypes>
+interface SetActiveTagAction {
+    type: typeof SET_ACTIVE_TAG,
+    payload: {
+        genr: Array<IFilter>,
+        company: Array<IFilter>
+        year: Array<IFilter>
+    }
+}
+
+interface SetSearchDataAction {
+    type: typeof SET_SEARCH_DATA,
+    payload: {
+        key: string,
+        data: Array<IFilter>
+    }
+}
+
+export type SearchActionTypes = GetFilmsByNameAction | InitTagsAction | SetActiveTagAction | SetSearchDataAction
+
+export type SearchThunkType = ThunkAction<Promise<void>, RootState, unknown, SearchActionTypes>
