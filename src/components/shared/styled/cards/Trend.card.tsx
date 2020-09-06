18093ea3f3641342af0089bd41bf46ae9fd0d8b8
Vatamanyuk Mykola <mykola796@gmail.com>
2020-09-06 11:54:53 +0300

    trend card reworked

--- src/components/shared/styled/cards/Trend.card.tsx
+++ src/components/shared/styled/cards/Trend.card.tsx
@@ -1,49 +1,65 @@
 import React, { FC } from 'react'
-import styled from 'styled-components'
+import { useHistory } from 'react-router'
 
-import { FilmStyled } from './Film.card'
-
-import { IFilmTrend } from '../../../../interfaces/film/IFilm'
 import { Container } from '../../utils/layout'
 import { Text } from '../../utils/typography'
-import { ButtonText, ButtonWatch } from '../buttons/Buttons.shared'
 import { text } from '../../utils/colors'
 
-const TrendCardStyled = styled(FilmStyled)`
-    height: 80vh;
-    width: 100%;
-    
-    background-size: cover;    
+import { ButtonText } from '../buttons/Buttons.shared'
 
-    box-shadow: inset 0px -500px 20px rgba(0, 0, 0, 0.6);
-    
-    cursor: pointer;
+import { BgImgCard } from './Cards.shared'
 
-    &:hover {
-        box-shadow: inset 0px -500px 20px rgba(0, 0, 0, 0.6);
-    }
-`
+import { IFilmTrend } from '../../../../interfaces/film/IFilm'
 
-export const TrendCard: FC<IFilmTrend> = ({ wallpaper, name, describe, _id }) => 
-    <TrendCardStyled img={wallpaper}>
-        <Container justify='flex-start' p='10%' >
-            <Text
-                size='50px'
-                color='#fff'
-                weight='100'
+
+export const TrendCard: FC<IFilmTrend> = ({ wallpaper, name, describe, _id }) => {
+
+    const history = useHistory()
+
+    return (
+        <Container h='300px'>
+            <BgImgCard img={wallpaper} w='100%' h='100%' />
+            <Container 
+                w='40%' 
+                direction='column' 
+                justify='space-around' 
+                h='100%'  
+                p='20px' 
+                bgColor={text.dark}
             >
-                {name}
-            </Text>
+                <Container justify='flex-start'>
+                    <Text 
+                        size='17px' 
+                        spacing='1.3px' 
+                        uppercase
+                    >
+                        {name}
+                    </Text>
+                </Container>
+                <Container>
+                    <Text 
+                        style={{
+                            lineHeight: 2
+                        }}
+                        weight='100'
+                        spacing='1.1px'
+                        color='gray'
+                        size='9px'
+                    >
+                        {describe}
+                    </Text>
+                </Container>
+                <Container justify='flex-start'>
+                    <ButtonText 
+                        text='Go' 
+                        w='120px' 
+                        h='40px' 
+                        bgColor={text.blue} 
+                        textColor={text.dark} 
+                        onClick={() => history.push(`/film/${_id}`)}
+                    />
+                </Container>
+            </Container>
         </Container>
-        <Container w='40%' p='0 10%' >
-            <Text
-                size='10px'
-                weight='100'
-                spacing='1.1px'
-                color='silver'
-                style={{ lineHeight: '20px' }}
-            >
-                {describe}
-            </Text>
-        </Container>
-    </TrendCardStyled>
+    )
+}
