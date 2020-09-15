759a811c10b7b496e3bc422242e087c7d9ecdba2
Vatamanyuk Mykola <mykola796@gmail.com>
2020-09-15 15:19:13 +0300

    header title added

--- src/components/navigation/Header.tsx
+++ src/components/navigation/Header.tsx
@@ -4,15 +4,21 @@ import { Container } from '../shared/utils/layout'
 
 import { ButtonBack } from '../shared/styled/buttons/Buttons.shared'
 import { useHistory } from 'react-router'
+import { TextT } from '../shared/utils/typography'
 
-export const Header: FC = () => {
+interface IHeaderProps {
+    title?: string
+}
+
+export const Header: FC<IHeaderProps> = ({ title }) => {
     
     const history = useHistory()
     
     return (
-        <Container p='10px 0'>
+        <Container>
             <Container justify='flex-start' p='10px'>
                 <ButtonBack onClick={() => history.goBack()} />
+                <TextT>{title}</TextT>
             </Container>
         </Container>
     )
