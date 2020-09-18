c35579ee2bf7859afa1a298ea223d2d3e9f42cf1
Vatamanyuk Mykola <mykola796@gmail.com>
2020-09-19 00:53:47 +0300

    rework auth hook

--- src/hooks/auth/useAuth.ts
+++ src/hooks/auth/useAuth.ts
@@ -1,28 +1,41 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
+import { ipcRenderer } from 'electron'
 import { useDispatch } from 'react-redux'
 
 import { authStatus } from '../../redux/auth/actions'
 
 export const useAuth = () => {
 
+    const [loading, setLoading] = useState(true)
+
     const dispatch = useDispatch()
 
-    const login = (jwt: string) => {
-        localStorage.setItem('token', jwt)
-        dispatch(authStatus(true))
+    const getToken = () => new Promise(resolve => {
+        ipcRenderer.send('auth:token-get')
+        
+        ipcRenderer.on('auth:token', (_, token) => resolve(token))
+    })
+
+    const logout = () => {
+        ipcRenderer.send('auth:logout')
+        dispatch(authStatus(false))
     }
 
-    const logout = () => localStorage.removeItem('token')
-    
     useEffect(() => {
-        const token = localStorage.getItem('token')
-        
-        token && login(token)        
-    
-    }, [login])
+        (async () => {
+            
+            const token = await getToken()
+            
+            token && dispatch(authStatus(true))
+            
+            setLoading(false)
+
+        })()
+    }, [])
 
     return {
-        login,
+        loading,
+        getToken,
         logout
     }
 }
