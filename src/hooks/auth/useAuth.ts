e1e948085961151d56e32520581ffdea21012cda
Vatamanyuk Mykola <mykola796@gmail.com>
2020-09-20 19:30:36 +0300

    refactor auth hook

--- src/hooks/auth/useAuth.ts
+++ src/hooks/auth/useAuth.ts
@@ -4,13 +4,16 @@ import { useDispatch } from 'react-redux'
 
 import { authStatus } from '../../redux/auth/actions'
 
+type Token = string | null
+
 export const useAuth = () => {
 
     const [loading, setLoading] = useState(true)
+    const [token, setToken] = useState<Token>(null)
 
     const dispatch = useDispatch()
 
-    const getToken = () => new Promise(resolve => {
+    const getToken = () => new Promise<Token>(resolve => {
         ipcRenderer.send('auth:token-get')
         
         ipcRenderer.on('auth:token', (_, token) => resolve(token))
@@ -28,13 +31,16 @@ export const useAuth = () => {
             
             token && dispatch(authStatus(true))
             
+            
             setLoading(false)
+            setToken(token)
 
         })()
     }, [])
 
     return {
         loading,
+        token,
         getToken,
         logout
     }
