d4fbb3f6e63bef203fb8c4dac0afe3e10f1a5d0d
Vatamanyuk Mykola <mykola796@gmail.com>
2020-09-20 19:32:42 +0300

    rework signup page

--- src/pages/Auth/SignUp.tsx
+++ src/pages/Auth/SignUp.tsx
@@ -1,14 +1,16 @@
 import React, { FC, useState, ChangeEvent, FormEvent } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Redirect } from 'react-router'
+import { Redirect, useHistory } from 'react-router'
 
 import { Input } from '../../components/shared/styled/inputs/inputs.shared'
-import { PageLayout, Container } from '../../components/shared/utils/layout'
+import { Container } from '../../components/shared/utils/layout'
 import { ButtonText } from '../../components/shared/styled/buttons/Buttons.shared'
+import { TextT } from '../../components/shared/utils/typography'
 import { text } from '../../components/shared/utils/colors'
 
 import { signUp } from '../../redux/auth/actions'
 import { RootState } from '../../redux/rootReducer'
+
 
 export const SignUp: FC = () => {
 
@@ -21,6 +23,7 @@ export const SignUp: FC = () => {
 
     const dispatch = useDispatch()
     const isSignUp = useSelector((state: RootState) => state.auth.signUpStatus)
+    const history = useHistory()
 
     const _inputHandler = (event: ChangeEvent<HTMLInputElement>) => setForm({...form, [event.target.name]: event.target.value})
 
@@ -34,60 +37,64 @@ export const SignUp: FC = () => {
 
     if(isSignUp) return <Redirect to='/signin' />
 
-    return (
-        <PageLayout>
-            <form>
-                <Container 
-                    direction='column' 
-                    m='5% 0'
-                    h='90vh'
-                    justify='space-around'
-                >
-                    <Input 
-                        type='email'
-                        name='email'
-                        change={_inputHandler}
-                        w='50%' 
-                        placeholder='Email'
-                        p='3%'
-                    />
-                    <Input 
-                        type='text'
-                        name='username'
-                        change={_inputHandler}
-                        w='50%' 
-                        placeholder='Username'
-                        p='3%'
-                    />
-                    <Input 
-                        type='password'
-                        name='password'
-                        change={_inputHandler}
-                        w='50%' 
-                        placeholder='Password'
-                        p='3%'
-                    />
-                    <Input 
-                        type='password'
-                        name='confirmPassword'
-                        change={_inputHandler}
-                        w='50%' 
-                        placeholder='Confirm password'
-                        p='3%'
-                    />
-                    <Container w='55%' justify='flex-end'>
-                        <ButtonText 
-                            text='Sign Up' 
-                            bgColor={text.blue}
-                            h='45px'
-                            w='125px'
-                            textColor={text.dark}
-                            onClick={_submit} 
-                        />   
-                    </Container>
+    return <>
+            <Container 
+                direction='column' 
+                h='90vh'
+                justify='space-around'
+                w='500px'
+                m='0 auto'
+            >
+                <Container justify='flex-start' w='90%'>
+                    <TextT>Sign Up</TextT>
                 </Container>
-            </form>
-        </PageLayout>
-        
-    )
+                    <form style={{ height: '50%', width: '100%' }}>
+                        <Container direction='column' h='100%' justify='space-between'>
+                            {
+                                inputs.map((input, index) => 
+                                    <Input
+                                        key={index} 
+                                        w='60%' 
+                                        p='20px'
+                                        type={input.type} 
+                                        name={input.name}
+                                        placeholder={input.placeholder || input.name}
+                                        change={_inputHandler}
+                                    />
+                                )
+                            }
+                        </Container>
+                    </form>
+                <Container h='10%' justify='space-around' w='60%'>
+                    <ButtonText 
+                        text='Sign Up' 
+                        w='100%' 
+                        bgColor={text.blue} 
+                        h='40px' 
+                        onClick={_submit} 
+                    />
+                </Container>
+            </Container>
+        </> 
 }
+   
+
+const inputs = [
+    {  
+        name: 'email',
+        type: 'email',
+    },
+    {  
+        name: 'username',
+        type: 'text',
+    },
+    {  
+        name: 'password',
+        type: 'password',
+    },
+    {  
+        name: 'confirmPassword',
+        type: 'password',
+        placeholder: 'confirm password'
+    }
+]
