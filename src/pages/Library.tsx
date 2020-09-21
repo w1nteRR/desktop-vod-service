e023e3a6ad12b3777e81eee1bbce51e13e76e5de
Vatamanyuk Mykola <mykola796@gmail.com>
2020-09-21 16:38:13 +0300

    add library page

--- src/pages/Library.tsx
+++ src/pages/Library.tsx
@@ -1,16 +1,49 @@
 import React, { FC } from 'react'
+import { useSelector } from 'react-redux'
+import { useHistory } from 'react-router'
+import { ContinueWatching } from '../components/Library/ContinueWatching'
+import { LibraryView } from '../components/Library/Library.view'
+import { WatchLater } from '../components/Library/WatchLater'
+import { ButtonText } from '../components/shared/styled/buttons/Buttons.shared'
+import { text } from '../components/shared/utils/colors'
 
-import { Container } from '../components/shared/utils/layout'
+import { PageLayout, Container } from '../components/shared/utils/layout'
+import { Text, Title } from '../components/shared/utils/typography'
+import { useAuth } from '../hooks/auth/useAuth'
 
 export const Library: FC = () => {
-    return (
-        <Container 
-            bgColor='#343131' 
-            w='80%'
-            m='0 auto'
-            h='90vh'
-        >
-            <p>Lib</p>
+
+    const { token, loading } = useAuth()
+
+    const history = useHistory()
+
+    if(loading) return <></>
+
+    if(!token) return (
+        <Container h='50vh' direction='column' w='75%' m='0 auto'>
+            <Container m='10px'>
+                <Title>Library is available only to authorized users</Title>
+            </Container>
+            <ButtonText 
+                text='Sign In'
+                w='50%'
+                h='50px'
+                bgColor={text.blue}
+                onClick={() => history.push('/signin')}
+            />
         </Container>
     )
+
+    return <>
+        <div style={main}>
+            <ContinueWatching />
+            <WatchLater token={token} />
+        </div>
+    </>
+}
+
+const main = {
+    width: '75%',
+    margin: '0 auto',
+    backgroundColor: '#090909'
 }
