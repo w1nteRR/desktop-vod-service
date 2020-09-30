cf2d1c219ea1594925e2760155944faa7c0c3dee
Vatamanyuk Mykola <mykola796@gmail.com>
2020-09-30 16:05:50 +0300

    refactor cast component

--- src/components/Film/Cast/Cast.tsx
+++ src/components/Film/Cast/Cast.tsx
@@ -8,18 +8,24 @@ import { Title } from '../../shared/utils/typography'
 
 import { cast_cfg } from '../../../utils/configs/carousel'
 import { IActor } from '../../../interfaces/cast/IActor'
+import { text } from '../../shared/utils/colors'
 
 interface ICastProps {
     cast: Array<IActor>
 }
 
 export const Cast: FC<ICastProps> = ({ cast }) => 
-    <div style={{ margin: '50px 0'}}>
+    <div style={{
+        borderRadius: 5,
+        background: text.dark,
+        padding: 10,
+        margin: '20px 0'
+    }}>
         <Container justify='flex-start'>
             <Title>Cast</Title>
         </Container>
-        <div style={{ margin: '0 auto', padding: '30px 10px' }}>
-            <Carousel name='' config={cast_cfg} rows={1} toShow={4} toScroll={4}>
+        <div style={{ margin: '0 auto' }}>
+            <Carousel name='' config={cast_cfg} rows={1} toShow={3} toScroll={3}>
                 {
                     cast.map(actor => <CastCard key={actor._id} actorName={actor.actorName} films={actor.films} />)
                 }
