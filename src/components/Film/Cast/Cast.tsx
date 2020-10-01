e7fcd8c969ffc78ab955cc35b61b131d0765903e
Vatamanyuk Mykola <mykola796@gmail.com>
2020-10-01 17:34:18 +0300

    fix cast

--- src/components/Film/Cast/Cast.tsx
+++ src/components/Film/Cast/Cast.tsx
@@ -2,24 +2,37 @@ import React, { FC } from 'react'
 
 import { Carousel } from '../../Carousels/Carousel'
 
-import { CastCard } from '../../shared/styled/cards/Cast.card'
-import { FilmLayoutCard } from '../../shared/styled/cards/Cards.shared'
+import { BgImgCard, FilmLayoutCard } from '../../shared/styled/cards/Cards.shared'
 
-import { IActor } from '../../../interfaces/cast/IActor'
+import { Container } from '../../shared/utils/layout'
+import { Text } from '../../shared/utils/typography'
 
 import { cast_cfg } from '../../../utils/configs/carousel'
+import { IActor } from '../../../interfaces/cast/IActor'
 
 
 interface ICastProps {
     cast: Array<IActor>
 }
 
-export const Cast: FC<ICastProps> = ({ cast }) => 
+export const Cast: FC<ICastProps> = ({ 
+    cast 
+}) => 
     <FilmLayoutCard title='cast'>
         <div style={{ margin: '0 auto' }}>
             <Carousel name='' config={cast_cfg} rows={1} toShow={3} toScroll={3}>
                 {
-                    cast.map(actor => <CastCard key={actor._id} actorName={actor.actorName} films={actor.films} />)
+                    cast.map(actor => 
+                        <>
+                        <BgImgCard key={actor._id} img={actor.films.img} m='10px' h='200px' />                           
+                            <Container direction='column' h='100%' justify='flex-end'>
+                                <Text size='13px' weight='600'>{actor.films.actorRole}</Text>
+                            <Container m='10px'>
+                                <Text color='#fff' size='12px'>{actor.actorName}</Text>
+                            </Container>
+                        </Container>
+                        </>
+                    )
                 }
             </Carousel>
         </div>
