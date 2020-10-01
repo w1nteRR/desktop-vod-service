fc0b0c815d4320b8a5dc3ad454799f9415005836
Vatamanyuk Mykola <mykola796@gmail.com>
2020-10-01 12:52:09 +0300

    add film card

--- src/components/shared/styled/cards/Cards.shared.tsx
+++ src/components/shared/styled/cards/Cards.shared.tsx
@@ -1,4 +1,8 @@
 import React, { FC } from 'react'
+import { text } from '../../utils/colors'
+
+import { Container } from '../../utils/layout'
+import { Title } from '../../utils/typography'
 
 import { BgImgCardS } from './Cards.styled'
 
@@ -10,8 +14,35 @@ interface ICardProps {
     onClick?: () => void
 }
 
-export const BgImgCard: FC<ICardProps> = ({ w, h, m, img, children, onClick }) => 
+
+export const BgImgCard: FC<ICardProps> = ({ 
+    w, 
+    h, 
+    m, 
+    img, 
+    children, 
+    onClick 
+}) => 
     <BgImgCardS w={w} h={h} img={img} m={m} onClick={onClick}>
         {children}
     </BgImgCardS>
 
+
+
+export const FilmLayoutCard: FC<{ title: string }> = ({
+    title,
+    children
+}) => 
+    <div style={{
+        borderRadius: 5,
+        background: text.dark,
+        width: '100%',
+        margin: '20px 0'
+    }}> 
+        <Container justify='flex-start'>
+            <Title style={{
+                margin: 20
+            }}>{title}</Title>
+        </Container>
+        {children}
+    </div>
