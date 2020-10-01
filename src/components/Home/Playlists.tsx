7ce4a6f684daea3deaa93a8c1baee8e42adeec7c
Vatamanyuk Mykola <mykola796@gmail.com>
2020-10-01 17:33:58 +0300

    fix playlists

--- src/components/Home/Playlists.tsx
+++ src/components/Home/Playlists.tsx
@@ -1,25 +1,41 @@
 import React, { FC } from "react"
+import { useHistory } from "react-router"
 
 import { Carousel } from '../Carousels/Carousel'
-import { FilmCard } from '../shared/styled/cards/Film.card'
 
-import { IPlaylist } from '../../interfaces/playlist/IPlaylist'
+import { BgImgCard } from "../shared/styled/cards/Cards.shared"
 
 import { film_cfg } from '../../utils/configs/carousel'
+import { IPlaylist } from '../../interfaces/playlist/IPlaylist'
+
 
 interface IPlaylistsProps {
     playlists: Array<IPlaylist>
 }
 
-export const Playlists: FC<IPlaylistsProps> = ({ playlists }) => 
-    <>
+export const Playlists: FC<IPlaylistsProps> = ({ 
+    playlists 
+}) => {
+
+    const history = useHistory()
+
+    return <>
         {
             playlists.map(playlist => 
                 <Carousel key={playlist._id} name={playlist.name} config={film_cfg} rows={1} toShow={3} toScroll={3}>
                     {
-                        playlist.films.map(film => <FilmCard key={film._id} _id={film._id} img={film.img} />)
+                        playlist.films.map(film => 
+                            <BgImgCard 
+                                key={film._id}
+                                onClick={() => history.push(`/film/${film._id}`)} 
+                                img={film.img} 
+                                m='5px' 
+                            />
+                        )
                     }
                 </Carousel>
             )
         }
     </>
+} 
+    
