e74b603ca2d06d9b79f369aec3163af004ee00fd
Vatamanyuk Mykola <mykola796@gmail.com>
2020-10-01 12:52:26 +0300

    fix seasons

--- src/components/Film/Series/Seasons.tsx
+++ src/components/Film/Series/Seasons.tsx
@@ -1,34 +1,31 @@
 import React, { FC } from 'react'
 
 import { Container } from '../../shared/utils/layout'
-import { Title, Text } from '../../shared/utils/typography'
+import { Text } from '../../shared/utils/typography'
 
 interface ISeasonsProps {
     seasons: Array<number>
     onSeasonClick: (n: number) => void
 }
 
-export const Seasons: FC<ISeasonsProps> = ({ seasons, onSeasonClick }) => {
-    return (
-        <Container justify='space-between'>
-            <Container justify='flex-start'>
-                <Title>Series</Title>
-            </Container>
-        <Container justify='flex-end' w='90%' p='10px'>
-            {
-                seasons.map(num => 
-                    <Text 
-                        key={num} 
-                        uppercase
-                        color='silver'
-                        style={{ marginLeft: 20, cursor: 'pointer' }}
-                        onClick={() => onSeasonClick(num)}
-                    >
-                        Season {num}
-                    </Text>
-                )
-            }
-        </Container>
+export const Seasons: FC<ISeasonsProps> = ({ 
+    seasons, 
+    onSeasonClick 
+}) => 
+    <Container justify='flex-end' w='95%' m='0 auto'>
+        {
+            seasons.map(num => 
+                <Text 
+                    key={num} 
+                    uppercase
+                    size='9px'
+                    weight='900'
+                    color='silver'
+                    style={{ marginLeft: 20, cursor: 'pointer' }}
+                    onClick={() => onSeasonClick(num)}
+                >
+                    Season {num}
+                </Text>
+            )
+        }
     </Container>
-    )
-}
