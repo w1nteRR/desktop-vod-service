6c16660b0c2fe0bf69de09cd01c30a391ad7e100
Vatamanyuk Mykola <mykola796@gmail.com>
2020-10-02 15:47:43 +0300

    fix wl api

--- src/api/library.api.ts
+++ src/api/library.api.ts
@@ -4,43 +4,58 @@ export const watchLaterApi = {
     remove: async (_id: string, token: string | null) => {
         try {
 
-            const result = await api.put('/api/library/watch-later/remove', {
-                _id,
-                token
+            const result = await api.put('/api/library/watch-later/remove', 
+            {
+                _id
+            },
+            {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
             })
 
             return result.data
 
         } catch (err) {
-            throw (err)
+            throw err
         }
     },
     add: async (_id: string, token: string | null) => {
         try {
 
-            const result = await api.put('/api/library/watch-later/add', {
-                _id,
-                token
+            const result = await api.put('/api/library/watch-later/add', 
+            {
+                _id
+            },
+            {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
             })
 
             return result.data
 
         } catch (err) {
-            throw (err)
+            throw err
         }
     },
     status: async (_id: string, token: string | null) => {
         try {
 
-            const result = await api.post('/api/library/watch-later/status', {
-                _id,
-                token
+            const result = await api.post('/api/library/watch-later/status', 
+            {
+                _id
+            }, 
+            {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }   
             })
 
             return result.data
 
         } catch (err) {
-            throw (err)
+            throw err
         }
     }
 }
