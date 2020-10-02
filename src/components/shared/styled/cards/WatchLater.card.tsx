b220025fd738b3837e31586dc5e60749c1231b24
Vatamanyuk Mykola <mykola796@gmail.com>
2020-10-02 13:37:37 +0300

    wl card rework

--- src/components/shared/styled/cards/WatchLater.card.tsx
+++ src/components/shared/styled/cards/WatchLater.card.tsx
@@ -1,67 +1,59 @@
 import React, { FC } from 'react'
 import Icon from '@mdi/react'
 import { mdiDelete } from '@mdi/js'
+import styled from 'styled-components'
 
 import { Container } from '../../utils/layout'
 import { text } from '../../utils/colors'
-
-import { ButtonText } from '../buttons/Buttons.shared'
+import { TextT } from '../../utils/typography'
 
 import { BgImgCard } from './Cards.shared'
 
 import { IFilmShort } from '../../../../interfaces/film/IFilm'
+
 
 interface IWatchLaterCardProps extends IFilmShort {
     onFilmClick: () => void
     onDeleteBtnClick: () => void
 }
 
+const DeleteHover = styled(Container)`
+    cursor: pointer;
+
+    width: 120px;
+    height: 100px;
+
+    z-index: 5;
+
+    &:hover {
+        background: #121212
+    }
+`
+
 export const WatchLaterCard: FC<IWatchLaterCardProps> = ({ 
     img, 
     onDeleteBtnClick,
-    onFilmClick 
+    onFilmClick,
+    name
 }) => 
-    <>
-        <Container 
-            w='95%'
-            h='170px' 
-            style={{ cursor: 'pointer' }}
-            bgColor={text.dark}
-            direction='column'
-        >
-            <Container 
-                justify='space-between' 
-                onClick={onFilmClick}
-            >
-                <BgImgCard img={img} h='170px' w='100%' />
+  <Container 
+        m='20px auto' 
+        brRadius='5px' 
+        w='70%' 
+        justify='space-between' 
+        bgColor={text.dark}
+        style={{
+            cursor: 'pointer'
+        }}
+    >
+        <Container w='100%' justify='space-between' onClick={onFilmClick}>
+            <BgImgCard img={img} h='100px' w='200px' />
+            <Container w='200px'>
+                <TextT>{name}</TextT>
             </Container>
         </Container>
-        <Container 
-            bgColor={text.dark} 
-            w='95%' 
-            m='10px 0' 
-            h='50px'
-            justify='space-around'
-            style={{
-                borderRadius: 5
-            }}
-        >
-        <ButtonText 
-            text='Watch' 
-            bgColor={text.blue} 
-            w='100px'                 
-            onClick={onFilmClick}
-        />
-            <Container 
-                h='70%'
-                w='20%'
-                style={{
-                    cursor: 'pointer'
-                }}
-            >
-                <Icon path={mdiDelete} size={.8} color={text.red} />
-            </Container>
-        </Container>
-    </>
-
+        <DeleteHover onClick={onDeleteBtnClick}>
+            <Icon path={mdiDelete} size={.8} color={text.red} />
+        </DeleteHover>
+    </Container>
             
