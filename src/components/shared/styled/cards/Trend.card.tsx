1267becfc34ac1b73b8fbc35c4333691549c2786
Vatamanyuk Mykola <mykola796@gmail.com>
2020-10-13 17:49:18 +0300

    rework trend card

--- src/components/shared/styled/cards/Trend.card.tsx
+++ src/components/shared/styled/cards/Trend.card.tsx
@@ -11,60 +11,68 @@ import { BgImgCard } from './Cards.shared'
 
 import { IFilmTrend } from '../../../../interfaces/film/IFilm'
 
-
-export const TrendCard: FC<IFilmTrend> = ({ wallpaper, name, describe, _id }) => {
+export const TrendCard: FC<IFilmTrend> = ({ 
+    wallpaper, 
+    name, 
+    describe, 
+    _id,
+    genr
+}) => {
 
     const history = useHistory()
 
     return (
-        <Container h='400px'>
+        <Container h='450px'>
             <BgImgCard img={wallpaper} w='100%' h='100%'>
                 <Container 
-                    bgColor={text.dark} 
                     w='inherit' 
                     h='inherit'
                     align='flex-start'
                     direction='column'
-                    style={{
-                        opacity: .4,
-                    }}
+                    justify='flex-end'
                 >
+                    <Container bgColor='' direction='column' align='flex-start'>
+                        <div style={{
+                            background: 'linear-gradient(180deg, rgba(50, 47, 47, .1) 0%, rgba(9, 9, 9, 0.92) 100%)',
+                            height: 450,
+                            width: '100%'
+                        }}>
+                            <Container h='100%' direction='column' justify='flex-end' align='flex-start'>
+                                <Container p='20px 50px' justify='flex-start' w='90%'>
+                                    <Text color='#fff' size='36px'>{name}</Text>
+                                </Container>
+                                <Container p='20px 50px' justify='flex-start' w='90%'>
+                                    {
+                                        genr.map((genr, index) => 
+                                            <Text 
+                                                key={index} 
+                                                size='10px' 
+                                                uppercase 
+                                                m='10px'
+                                                weight='900'
+                                            >
+                                                {genr}
+                                            </Text>
+                                        )
+                                    }
+                                </Container>
+                                <Container p='50px 0' justify='space-around'>
+                                    <Container w='60%'>
+                                        <Describe>{describe}</Describe>
+                                    </Container>
+                                    <ButtonText 
+                                        bgColor={text.blue}
+                                        w='160px'
+                                        h='50px'
+                                        text='Watch' 
+                                        onClick={() => history.push(`/film/${_id}`)}
+                                    />
+                                </Container>
+                            </Container>
+                        </div>
+                    </Container>
                 </Container>
             </BgImgCard>
-            <Container 
-                w='40%' 
-                direction='column' 
-                justify='space-around' 
-                h='100%'  
-                p='20px' 
-                bgColor={text.dark}
-            >
-                <Container justify='flex-start'>
-                    <Text 
-                        size='17px' 
-                        spacing='1.3px' 
-                        uppercase
-                    >
-                        {name}
-                    </Text>
-                </Container>
-                <Container>
-                    <Describe>
-                        {describe}
-                    </Describe>
-                </Container>
-                <Container justify='flex-start'>
-                    <ButtonText 
-                        brdrRadius='5px'
-                        text='Go' 
-                        w='120px' 
-                        h='40px' 
-                        bgColor={text.blue} 
-                        textColor={text.dark} 
-                        onClick={() => history.push(`/film/${_id}`)}
-                    />
-                </Container>
-            </Container>
         </Container>
     )
 }
