5d6604fc005a90d88cee578dcd414e1cefc0ba9b
Vatamanyuk Mykola <mykola796@gmail.com>
2020-10-13 17:49:56 +0300

    refactor home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,37 +1,12 @@
 import React, { FC } from 'react'
 
-import { PageLayout, Container } from '../components/shared/utils/layout'
-import { DualRing } from '../components/shared/styled/loaders/DualRing'
-
 import { Playlists } from '../components/Home/Playlists'
 import { Trends } from '../components/Home/Trends'
 
-import { IPlaylist } from '../interfaces/playlist/IPlaylist'
-import { useScrollLoader } from '../hooks/useScrollLoader'
+import { PageLayout } from '../components/shared/utils/layout'
 
-
-export const Home: FC = () => {
-
-    const { data } = useScrollLoader()
-
-    const playlists: Array<IPlaylist> = data
-
-    if(!playlists.length) return (
-        <Container h='100vh'>
-            <DualRing />
-        </Container>
-    )
-
-    return (      
-        <div style={main}>   
-            <Trends />   
-            <Playlists playlists={playlists} />
-        </div>
-    )
-}
-
-const main = {
-    width: '80%',
-    margin: '0 auto',
-    backgroundColor: '#090909'
-}
+export const Home: FC = () =>      
+    <PageLayout>   
+        <Trends />   
+        <Playlists />
+    </PageLayout>
